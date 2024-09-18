d1d9b5711ac14e88641b9f91024e7961af725803
Susdump <susmail0022@gmail.com>
2024-09-18 10:24:45 +0530

    Add files via upload

---
+++ filehandling_pract11.cpp
@@ -0,0 +1,128 @@
+# include<iostream>
+# include<fstream>
+using namespace std;
+
+class student{
+    int roll;
+    char div;
+    string name;
+    string address;
+    public:
+    void accept(){
+        cout<<"Enter the name of the student: \n";
+        cin>>name;
+        cout<<"Enter the roll no. of the student: \n";
+        cin>>roll;
+        cout<<"Enter the div of the student: \n";
+        cin>>div;
+        cout<<"Enter the address of the student: \n";
+        cin>>address;
+    }
+    void display(){
+        cout<<"\t"<<name<<"\t"<<roll<<"\t"<<div<<"\t"<<address<<"\n";
+    }
+    bool checkr(int r){
+        if (r == roll)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+        return false;
+        }
+};
+
+void addrecord(){
+cout<<"Enter the number of the records you want to add: ";
+int n; cin>>n;
+student s;
+ofstream o("student.txt");
+
+for (int i = 0; i < n; i++)
+{
+    s.accept();
+    o.write((char *)&s,sizeof(s));
+}
+o.close();
+}
+
+void addnewrecord(){
+cout<<"Enter the number of the records you want to add: ";
+int n; cin>>n;
+student s;
+ofstream o("student.txt", ios::app);
+for (int i = 0; i < n; i++)
+{
+    s.accept();
+    o.write((char *)&s,sizeof(s));
+}
+o.close();
+}
+void showbyroll(){
+    int r;
+    cout<<"Enter the Roll to be checked in records\n";
+    cin>>r;
+    bool flag = false;
+    ifstream a("student.txt");
+    student s;
+    while (a.read((char *)&s,sizeof(s)))
+    {
+        if(s.checkr(r)){
+            cout << "Name \t" << " roll \t" << "div \t" << "address \n";
+            s.display();
+            flag = true;
+        }
+    }
+    if (flag == false)
+    {
+        cout<<"The roll no. doesn't exist in the records \n";
+    }
+    
+a.close();
+}
+void displayrecord(){
+ifstream a("student.txt");
+student s;
+cout<<"Name \t"<<" roll \t"<< "div \t"<< "address \n";
+while (a.read((char *)&s,sizeof(s)))
+{
+    s.display();
+}
+a.close();
+}
+
+int main(){
+    int ch = 0;
+    while (ch != 5)
+    {
+        cout << "\nSelect option from the below: \n"
+             << "1. To Add Record into the file: \n"
+             << "2. To Display Records from the file: \n"
+             << "3. To Add new Records to the file: \n"
+             << "4. Check if roll no exist and display: \n"
+             << "5. Exit from the program: \n";
+        cin>>ch;
+        switch (ch)
+        {
+        case 1:
+            addrecord();
+            break;
+        case 2:
+            displayrecord();
+            break;
+        case 3:
+            addnewrecord();
+            break;
+        case 4:
+            showbyroll();
+            break;
+        case 5:
+            break;
+        default:
+            cout << "Select correct option\n";
+            break;
+        }
+}
+    return 0;
+}
